526249ed8b7f82702f8173808500b7ddb2cf1e55
Igor Sodre <csodreigor@gmail.com>
2022-02-12 17:42:15 -0300

    add specs for StcokResultFormater

---
+++ tests/Tests.Bot.Unit/StockResultFormatterTests.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using StockChatApp.Bot.Models;
+using StockChatApp.Bot.Services;
+using Xunit;
+
+namespace Tests.Bot.Unit;
+
+public class StockResultFormatterTests
+{
+    private readonly StockResultFormatter _sut = new StockResultFormatter();
+
+    [Fact]
+    public void FormatStock_ReturnCorrectFormattedString()
+    {
+        // Arrange
+        var stock = new StockResult
+        {
+            Symbol = "TestSymbol",
+            Open = 29.50M
+        };
+
+        // Act
+        var result = _sut.FormatStock(stock);
+
+        // Assert
+        result.Should().Be("TestSymbol quote is $29.50 per share");
+    }
+}
